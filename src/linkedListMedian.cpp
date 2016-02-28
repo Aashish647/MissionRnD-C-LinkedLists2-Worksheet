54dfe47f72a5436ce8ab9f0ee1cdafb97aa6f6c2
aashish647 <aashish647@missionrnd.com>
2016-02-28 01:18:27 -0800

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -20,5 +20,35 @@ struct node {
 };
 
 int linkedListMedian(struct node *head) {
-	return -1;
+	if (head == NULL)
+	{
+		return -1;
+	}
+	struct node *ptr1 = head;
+	struct node *ptr2 = head;
+	struct node *ptr3 = head;
+	int count = 0, temp = 0, sum = 0;
+	while ((ptr1->next != NULL)&& (ptr2->next!=NULL))
+	{
+		temp = ptr2->num;
+		ptr1 = ptr1->next->next;
+		ptr2 = ptr2->next;
+		count++;
+	}
+	if ((count % 2) == 1)
+	{
+		while (ptr3->next != ptr2)
+		{
+			ptr3 = ptr3->next;
+		}
+		sum = ptr3->num;
+		temp = ptr2->num;
+		sum = sum + temp;
+		sum = sum / 2;
+		return sum-1;
+	}
+	else
+	{
+		return ptr2->num;
+	}
 }
